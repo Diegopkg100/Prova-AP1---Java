a8924a64d44bddedd5cc345cf15036fc717730f1
Diegopkg100 <diegopkg100@gmail.com>
2025-04-02 14:12:08 -0300

    Add files via upload

---
+++ Exercicio 2 - AP1/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("DIGITE A TEMPERATURA EM CELSIUS");
+
+        double temperaturaC = sc.nextDouble();
+
+        double conta = temperaturaC * 1.8 + 32 ;
+
+        System.out.printf("%.2f FAHRENHEIT", conta);
+        sc.close();
+    }
+}
